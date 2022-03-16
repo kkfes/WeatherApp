5f2084edc69206fa3b95e27bb6bbe66051983af6
kkfes <musiks005@gmail.com>
2022-03-16 15:39:58 +0600

    start

--- app/src/main/java/com/example/weather/adapters/HourlyTempAdapter.java
+++ app/src/main/java/com/example/weather/adapters/HourlyTempAdapter.java
@@ -61,7 +61,7 @@ public class HourlyTempAdapter extends RecyclerView.Adapter<HourlyTempAdapter.Ho
         }
 
         public String firstUpperCase(String word) {
-            if (word == null || word.isEmpty()) return "";//или return word;
+            if (word == null || word.isEmpty()) return "";
             return word.substring(0, 1).toUpperCase() + word.substring(1);
         }
     }
